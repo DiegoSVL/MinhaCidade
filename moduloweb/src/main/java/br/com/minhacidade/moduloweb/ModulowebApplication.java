512662d4732ca2578875a2be1b8acdb8f2ea2460
Diego Laurentino <diegosvlaurentino@gmail.com>
2024-05-09 18:41:47 -0300

    Commit inicial

---
+++ moduloweb/src/main/java/br/com/minhacidade/moduloweb/ModulowebApplication.java
@@ -0,0 +1,13 @@
+package br.com.minhacidade.moduloweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulowebApplication.class, args);
+	}
+
+}
---
+++ moduloweb/src/test/java/br/com/minhacidade/moduloweb/ModulowebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.minhacidade.moduloweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
